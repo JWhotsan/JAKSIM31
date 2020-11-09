61f7865c88d8a87975873f8c736c5b63ded401c8
Jeong Jinwoo <zizzoonn140@gmail.com>
2020-11-09 22:21:44 +0900

    기능: footer(frontend)

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import '../style/Footer.scss';
 
 const Footer = () => {
     return(
         <div className="footer">
-            footer
+            <div style={{width: '100%', height: '1px', background: '#ddd'}}></div>
+            <div className="container">
+                <footer>
+                    <div className="footer-copyright" style={{textAlign: 'center'}}>Copyright ⓒ 2020 Jaksim 31. All rights reserved</div>
+                    <div className="footer-menu">
+                        <a href="terms">이용약관</a>
+                        <a href="/privacy">개인정보 처리방침</a>
+                        <a href="faq">자주 묻는 질문</a>
+                    </div>
+                </footer>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
